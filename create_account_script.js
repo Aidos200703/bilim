c07a6ad60d0f599390dd224a9f4fca24ac0d7ce7
aidos <aidosonalbaev03@gmail.com>
2025-07-05 15:27:17 +0500

    Add files via upload

---
+++ create_account_script.js
@@ -0,0 +1,23 @@
+function togglePassword() {
+    const input = document.getElementById('passwordInput');
+    const icon = document.getElementById('eyeIcon');
+    if (input.type === 'password') {
+        input.type = 'text';
+        icon.src = 'img/eye-icon.png'; // Ашық көз иконкасы
+    } else {
+        input.type = 'password';
+        icon.src = 'img/eye-off-icon.png'; // Жабық көз иконкасы
+    }
+}
+
+function togglePasswordRepeat() {
+    const input = document.getElementById('passwordInputRepeat');
+    const icon = document.getElementById('eyeIconRepeat');
+    if (input.type === 'password') {
+        input.type = 'text';
+        icon.src = 'img/eye-icon.png'; // Ашық көз иконкасы
+    } else {
+        input.type = 'password';
+        icon.src = 'img/eye-off-icon.png'; // Жабық көз иконкасы
+    }
+}
